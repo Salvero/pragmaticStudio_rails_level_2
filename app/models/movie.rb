0f7f3b165187ca02896fa23fdfe24c228789d6b5
Salman Akram <thunder9ruby@gmail.com>
2015-06-19 01:56:53 -0400

    add one to many relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,8 @@ class Movie < ActiveRecord::Base
 		message: "must reference a GIF, JPG, or PNG image"		
 	}
 
+	has_many :reviews, dependent: :destroy
+
 	RATINGS = %w(G PG PG-13 R NC-17)
 	validates :rating, inclusion: { in: RATINGS }
 
