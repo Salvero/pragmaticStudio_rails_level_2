c81558f4b21a10d92aec56910b3217a4ce5bf1be
Salman Akram <thunder9ruby@gmail.com>
2015-06-22 14:24:48 -0400

    create to display review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,8 @@ class ReviewsController < ApplicationController
 		@movie = Movie.find(params[:movie_id])
 		@reviews = @movie.reviews
 	end
+	def new
+		@movie = Movie.find(params[:movie_id])
+		@review = @movie.reviews.new
+	end
 end
