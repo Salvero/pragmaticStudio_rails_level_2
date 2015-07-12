afcff83f14642bc2b5a1c554cbf04a6504f9c8d9
Salman Akram <thunder9ruby@gmail.com>
2015-07-12 02:41:09 -0400

    create signing out spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -32,6 +32,7 @@ describe "Signing in" do
 		expect(page).to have_link(user.name)
 		expect(page).not_to have_link('Sign In')
 		expect(page).not_to have_link('Sign Up')
+		expect(page).to have_link('Sign Out')
 	end
 
 	it "does not sign in the user if the email/password combination is invalid" do
@@ -50,6 +51,7 @@ describe "Signing in" do
 
 		expect(page).not_to have_link(user.name)
 		expect(page).to have_link('Sign In')
-		expect(page).to have_link('Sign Up')		
+		expect(page).to have_link('Sign Up')
+		expect(page).not_to have_link('Sign Out')
 	end
 end
