5c88b422942dccbc3b1c05d61a52b825a78e9e94
Salman Akram <thunder9ruby@gmail.com>
2015-07-19 01:21:13 -0400

    add authorization spec

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -7,6 +7,8 @@ describe "Viewing the list of users" do
 		user2 = User.create!(user_attributes(name: "Moe", email: "moe@example.com"))
 		user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com"))
 
+		sign_in(user)
+
 		visit users_url
 
 		expect(page).to have_link(user1.name)
