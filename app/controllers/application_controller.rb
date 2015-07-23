b50cdc6a3ae18f8a0159d0086fa0197650a0a989
Salman Akram <thunder9ruby@gmail.com>
2015-07-23 02:43:55 -0400

    add admin column to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ private
 		end
 	end
 
+	def require_admin
+		unless current_user_admin?
+			redirect_to root_url, alert: "Unauthorized access!"
+		end
+	end
+
+	def current_user_admin?
+		current_user && current_user_admin?
+	end
+
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
