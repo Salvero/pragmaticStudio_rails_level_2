7efdc3f3c9719ce898eaa5e90292ca381045a112
Salman Akram <thunder9ruby@gmail.com>
2015-08-04 02:00:46 -0400

    remove duplication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class FavoritesController < ApplicationController
 	before_action :require_signin
+	before_action :set_movie
 
 	def create
-		@movie = Movie.find(params[:movie_id])
 		@movie.favorites.create!(user: current_user)
 
 		redirect_to @movie, notice: "Thanks for fav'ing!"
 	end
 
 	def destroy
-		@movie = Movie.find(params[:movie_id])
 		favorite = current_user.favorites.find(params[:id])
 		favorite.destroy
 		redirect_to @movie, notice: "Successfully unfave movie"
 	end
+
+	private
+
+		def set_movie
+			@movie = Movie.find(params[:movie_id])
+		end		
+
 end
