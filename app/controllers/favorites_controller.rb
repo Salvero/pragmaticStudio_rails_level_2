d28f418dd77e6e0d3ea54b8252d5619da907280a
Salman Akram <thunder9ruby@gmail.com>
2015-08-04 01:58:21 -0400

    make unfave button work

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,17 @@
 class FavoritesController < ApplicationController
 	before_action :require_signin
-	
+
 	def create
 		@movie = Movie.find(params[:movie_id])
 		@movie.favorites.create!(user: current_user)
 
 		redirect_to @movie, notice: "Thanks for fav'ing!"
+	end
+
+	def destroy
+		@movie = Movie.find(params[:movie_id])
+		favorite = current_user.favorites.find(params[:id])
+		favorite.destroy
+		redirect_to @movie, notice: "Successfully unfave movie"
 	end
 end
