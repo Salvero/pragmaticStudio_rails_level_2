0e93d0ffcdd4e2777d03c8e4a680a0817ae2d2a2
Salman Akram <thunder9ruby@gmail.com>
2015-08-22 23:17:12 -0400

    add more listing of upcoming and recent movies buttons via sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'movies/filter/hits' => "movies#index", scope: "hits"
   # get 'movies/filter/flops' => "movies#index", scope: "flops"  
 
-  get 'movies/filter/:scope' => "movies#index"
+  get 'movies/filter/:scope' => "movies#index", as: :filtered_movies
 
   resources :users
 
